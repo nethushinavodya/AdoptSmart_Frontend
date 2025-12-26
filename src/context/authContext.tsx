d55d172ea14a8f83f49047f475b995f5634147d8
nethushinavodya <nethushiperera@gmail.com>
2025-12-26 13:23:25 +0530

    add AuthContext for managing user authentication state

---
+++ src/context/authContext.tsx
@@ -0,0 +1,47 @@
+import { createContext, useContext, useEffect, useState } from "react"
+import { getMyDetails } from "../services/auth"
+
+const AuthContext = createContext<any>(null)
+
+export const AuthProvider = ({ children }: any) => {
+  const [user, setUser] = useState<any>(null)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const token = localStorage.getItem("accessToken")
+    if (token) {
+      getMyDetails()
+          .then((res) => {
+            setUser(res.data)
+          })
+          .catch((err) => {
+            // if token expire or me api call fail
+            setUser(null)
+            console.error(err)
+          })
+          .finally(() => {
+            setLoading(false)
+          })
+    } else {
+      // eslint-disable-next-line react-hooks/set-state-in-effect
+      setUser(null)
+      setLoading(false)
+    }
+  }, [])
+
+  return (
+      <AuthContext.Provider value={{ user, setUser, loading }}>
+        {children}
+      </AuthContext.Provider>
+  )
+}
+
+// { user, setUser }
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuth = () => {
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error("useAuth must be used within an AuthProvider")
+  }
+  return context
+}
