7dbfa1aa188ab670b9847ec496ebbc24d1923884
nethushinavodya <nethushiperera@gmail.com>
2026-01-07 22:08:45 +0530

    refactor(api): update API base URL to production environment

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,10 @@
 import axios, { AxiosError } from "axios"
 import { refreshTokens } from "./auth"
+import dotenv from "dotenv"
+dotenv.config()
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + "/api/v1",
+  baseURL: process.env.VITE_API_URL + "/api/v1",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
